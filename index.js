17c0170c365180f706cb6649753da9627599e5f2
PromzzyKoncepts <okechukwupromise638@gmail.com>
2025-07-04 23:01:18 +0100

    fix cors issue

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const http = require('http');
-const socketIo = require('socket.io');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
 
-const streamRoutes = require('./routes/streamRoutes');
-const userRoutes = require('./routes/userRoutes');
-const commentRoutes = require('./routes/commentRoutes');
-const setupSocket = require('./configs/socket');
+const streamRoutes = require("./routes/streamRoutes");
+const userRoutes = require("./routes/userRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const setupSocket = require("./configs/socket");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+    origin: [
+      "https://jakp.vercel.app",
+      "http://localhost:3000",
+      "https://parenthub.lovetoons.org",
+      "https://jakp.lovetoons.org",
+      "https://lovetoons.org",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  },
 });
 
-// Middleware
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "https://jakp.vercel.app",
+    "http://localhost:3000",
+    "https://parenthub.lovetoons.org",
+    "https://jakp.lovetoons.org",
+    "https://lovetoons.org",
+    "*"
+  ],
+
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/comments', commentRoutes);
-app.use('/api/stream', streamRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/stream", streamRoutes);
 
 // Socket.io setup
 setupSocket(io);
